801be46f1f2f523dc957cc88164169bc6c9d74a9
Vladislav <v.groschkov@yandex.ru>
2021-10-19 15:02:33 +0300

    Lesson 19.10.2021

--- locallibrary/locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/locallibrary/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'azpxdbrjn79^m&o4p^uf+5#=-ymyno-yl9=k34zny-4u-!mi6v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
